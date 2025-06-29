6cb2c2e2708275bd0ac288de1edb27120af2d2bb
“Ishan <ishanbathla69@gmail.com>
2025-06-29 13:06:16 +0530

    Adding hardcoded mock data

---
+++ src/data/mockData.js
@@ -0,0 +1,4 @@
+export const users = [
+  { id: '1', role: 'Admin', email: 'admin@entnt.in', password: 'admin123' },
+  { id: '2', role: 'Patient', email: 'john@entnt.in', password: 'patient123', patientId: 'p1' },
+];
